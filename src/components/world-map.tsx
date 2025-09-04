3f95531c235da2d7ad9d4b58bdadcba02a0d4f61
Clyde Ian Acaso <ian.clyde24@gmail.com>
2025-09-04 19:53:05 +0800

    added css pin assets to cdn

--- src/components/world-map.tsx
+++ src/components/world-map.tsx
@@ -18,6 +18,14 @@ const FitBounds = ({ locations }: { locations: [number, number][] }) => {
   return null;
 };
 
+// Fix default marker icons (Leaflet + Vite)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
 // Heatmap layer
 const HeatmapLayer = ({ points }: { points: [number, number, number][] }) => {
   const map = useMap();
